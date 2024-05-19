51827328a775a86c80244bd7aa24e6119de46863
刘怀聪 <771901874@qq.com>
2024-05-19 09:19:21 +0800

    init:✨项目初始化提交

---
+++ README.md
@@ -0,0 +1,19 @@
+<p align="center">
+    <a href="" target="_blank">
+      <img src="./docs/imgs/icon.jpg" width="280" />
+    </a>
+</p>
+<h1 align="center">月亮勘探 - LunarSurvey</h1>
+<p align="center"><strong>基于 AI 快速制作并发布答题应用平台。<br><em>持续更新中～</em></strong></p>
+<div align="center">
+    <a href=""><img src="https://img.shields.io/badge/github-项目地址-yellow.svg?style=plasticr"></a>
+    <a href=""><img src="https://img.shields.io/badge/前端-项目地址-blueviolet.svg?style=plasticr"></a>
+</div>
+
+
+
+## 项目介绍🚀
+
+LunarSurvey（月亮勘探） 是一款基于 Vue 3 + Spring Boot + Redis + ChatGLM + RxJava + SSE 的 AI 答题应用平台。
+
+用户可以基于 AI 快速制作并发布答题应用，支持检索、分享、在线答题并基于 AI 得到回答总结；管理员可以集中管理和审核应用。
---
+++ src/main/java/com/cong/lunarsurvey/generate/CodeGenerator.java
@@ -0,0 +1,26 @@
+package com.cong.lunarsurvey.generate;
+
+import com.cong.lunarsurvey.model.entity.User;
+
+/**
+ * 代码生成器
+ *
+ * @author <a href="https://github.com/lhccong">聪</a>
+ */
+public class CodeGenerator {
+
+    /**
+     * 用法：追加process(数据类.class, "数据别名");
+     */
+    public static void main(String[] args) {
+        // 代码生成处理器
+        new GenerateProcessor()
+                // 生成项目路径
+                .packageName("com.cong.lunarsurvey")
+                // 排除字段策略
+                .exclusionStrategy("serialVersionUID", "isDelete","updateTime","createTime")
+                // 继续追加process(数据类.class, "数据别名")
+                .process(User.class, "用户");
+
+    }
+}
