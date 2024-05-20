6fc0b8adfa89fd25fdcda5d755561f3552974c1f
lhccong <liuhuaicong@zhut.cn>
2024-05-20 11:28:47 +0800

    feat: 🚀新增 题目 服务实现类

---
+++ src/main/java/com/cong/lunarsurvey/service/impl/QuestionServiceImpl.java
@@ -0,0 +1,174 @@
+package com.cong.lunarsurvey.service.impl;
+
+import cn.hutool.core.collection.CollUtil;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.cong.lunarsurvey.common.ErrorCode;
+import com.cong.lunarsurvey.constant.CommonConstant;
+import com.cong.lunarsurvey.exception.ThrowUtils;
+import com.cong.lunarsurvey.mapper.QuestionMapper;
+import com.cong.lunarsurvey.model.dto.question.QuestionQueryRequest;
+import com.cong.lunarsurvey.model.entity.App;
+import com.cong.lunarsurvey.model.entity.Question;
+import com.cong.lunarsurvey.model.entity.User;
+import com.cong.lunarsurvey.model.vo.QuestionVO;
+import com.cong.lunarsurvey.model.vo.UserVO;
+import com.cong.lunarsurvey.service.AppService;
+import com.cong.lunarsurvey.service.QuestionService;
+import com.cong.lunarsurvey.service.UserService;
+import com.cong.lunarsurvey.utils.SqlUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.ObjectUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+/**
+ * 题目服务实现
+ *
+ * @author <a href="https://github.com/lhccong">聪</a>
+
+ */
+@Service
+@Slf4j
+public class QuestionServiceImpl extends ServiceImpl<QuestionMapper, Question> implements QuestionService {
+
+    @Resource
+    private UserService userService;
+
+    @Resource
+    private AppService appService;
+
+    /**
+     * 校验数据
+     *
+     * @param question 题目
+     * @param add      对创建的数据进行校验
+     */
+    @Override
+    public void validQuestion(Question question, boolean add) {
+        ThrowUtils.throwIf(question == null, ErrorCode.PARAMS_ERROR);
+        // 从对象中取值
+        String questionContent = question.getQuestionContent();
+        Long appId = question.getAppId();
+        // 创建数据时，参数不能为空
+        if (add) {
+            // 补充校验规则
+            ThrowUtils.throwIf(StringUtils.isBlank(questionContent), ErrorCode.PARAMS_ERROR, "题目内容不能为空");
+            ThrowUtils.throwIf(appId == null || appId <= 0, ErrorCode.PARAMS_ERROR, "appId 非法");
+        }
+        // 修改数据时，有参数则校验
+        // 补充校验规则
+        if (appId != null) {
+            App app = appService.getById(appId);
+            ThrowUtils.throwIf(app == null, ErrorCode.PARAMS_ERROR, "应用不存在");
+        }
+    }
+
+
+    /**
+     * 获取查询包装器
+     *
+     * @param questionQueryRequest 问题查询请求
+     * @return {@link QueryWrapper }<{@link Question }>
+     */
+    @Override
+    public QueryWrapper<Question> getQueryWrapper(QuestionQueryRequest questionQueryRequest) {
+        QueryWrapper<Question> queryWrapper = new QueryWrapper<>();
+        if (questionQueryRequest == null) {
+            return queryWrapper;
+        }
+        // 从对象中取值
+        Long id = questionQueryRequest.getId();
+        String questionContent = questionQueryRequest.getQuestionContent();
+        Long appId = questionQueryRequest.getAppId();
+        Long userId = questionQueryRequest.getUserId();
+        Long notId = questionQueryRequest.getNotId();
+        String sortField = questionQueryRequest.getSortField();
+        String sortOrder = questionQueryRequest.getSortOrder();
+
+        // 补充需要的查询条件
+        // 模糊查询
+        queryWrapper.like(StringUtils.isNotBlank(questionContent), "questionContent", questionContent);
+        // 精确查询
+        queryWrapper.ne(ObjectUtils.isNotEmpty(notId), "id", notId);
+        queryWrapper.eq(ObjectUtils.isNotEmpty(id), "id", id);
+        queryWrapper.eq(ObjectUtils.isNotEmpty(appId), "appId", appId);
+        queryWrapper.eq(ObjectUtils.isNotEmpty(userId), "userId", userId);
+        // 排序规则
+        queryWrapper.orderBy(SqlUtils.validSortField(sortField),
+                sortOrder.equals(CommonConstant.SORT_ORDER_ASC),
+                sortField);
+        return queryWrapper;
+    }
+
+    /**
+     * 获取题目封装
+     *
+     * @param question 题目对象
+     * @return 题目封装
+     */
+    @Override
+    public QuestionVO getQuestionVO(Question question) {
+        // 对象转封装类
+        QuestionVO questionVO = QuestionVO.objToVo(question);
+
+        // 可以根据需要为封装对象补充值，不需要的内容可以删除
+        // region 可选
+        // 1. 关联查询用户信息
+        Long userId = question.getUserId();
+        User user = null;
+        if (userId != null && userId > 0) {
+            user = userService.getById(userId);
+        }
+        UserVO userVO = userService.getUserVO(user);
+        questionVO.setUser(userVO);
+        // endregion
+
+        return questionVO;
+    }
+
+    /**
+     * 分页获取题目封装
+     *
+     * @param questionPage 分页对象
+     * @return 分页对象
+     */
+    @Override
+    public Page<QuestionVO> getQuestionVOPage(Page<Question> questionPage) {
+        List<Question> questionList = questionPage.getRecords();
+        Page<QuestionVO> questionVOPage = new Page<>(questionPage.getCurrent(), questionPage.getSize(), questionPage.getTotal());
+        if (CollUtil.isEmpty(questionList)) {
+            return questionVOPage;
+        }
+        // 对象列表 => 封装对象列表
+        List<QuestionVO> questionVOList = questionList.stream().map(QuestionVO::objToVo).collect(Collectors.toList());
+
+        // 可以根据需要为封装对象补充值，不需要的内容可以删除
+        // region 可选
+        // 1. 关联查询用户信息
+        Set<Long> userIdSet = questionList.stream().map(Question::getUserId).collect(Collectors.toSet());
+        Map<Long, List<User>> userIdUserListMap = userService.listByIds(userIdSet).stream()
+                .collect(Collectors.groupingBy(User::getId));
+        // 填充信息
+        questionVOList.forEach(questionVO -> {
+            Long userId = questionVO.getUserId();
+            User user = null;
+            if (userIdUserListMap.containsKey(userId)) {
+                user = userIdUserListMap.get(userId).get(0);
+            }
+            questionVO.setUser(userService.getUserVO(user));
+        });
+        // endregion
+
+        questionVOPage.setRecords(questionVOList);
+        return questionVOPage;
+    }
+
+}
