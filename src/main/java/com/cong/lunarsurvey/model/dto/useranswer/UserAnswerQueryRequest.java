69854ff8712a2f2bcae1321e07fc8b5655073401
lhccong <liuhuaicong@zhut.cn>
2024-05-20 11:35:18 +0800

    fix: ✅查询用户答题记录请求 dto

--- src/main/java/com/cong/lunarsurvey/model/dto/useranswer/UserAnswerQueryRequest.java
+++ src/main/java/com/cong/lunarsurvey/model/dto/useranswer/UserAnswerQueryRequest.java
@@ -16,6 +16,61 @@ import java.io.Serializable;
 public class UserAnswerQueryRequest extends PageRequest implements Serializable {
 
     /**
+     *
+     */
+    private Long id;
+
+    /**
+     * 应用 id
+     */
+    private Long appId;
+
+    /**
+     * 应用类型（0-得分类，1-角色测评类）
+     */
+    private Integer appType;
+
+    /**
+     * 评分策略（0-自定义，1-AI）
+     */
+    private Integer scoringStrategy;
+
+    /**
+     * 用户答案（JSON 数组）
+     */
+    private String choices;
+
+    /**
+     * 评分结果 id
+     */
+    private Long resultId;
+
+    /**
+     * 结果名称，如物流师
+     */
+    private String resultName;
+
+    /**
+     * 结果描述
+     */
+    private String resultDesc;
+
+    /**
+     * 结果图标
+     */
+    private String resultPicture;
+
+    /**
+     * 得分
+     */
+    private Integer resultScore;
+
+    /**
+     * 用户 id
+     */
+    private Long userId;
+
+    /**
      * id
      */
     private Long notId;
@@ -24,11 +79,6 @@ public class UserAnswerQueryRequest extends PageRequest implements Serializable 
      * 搜索词
      */
     private String searchText;
-
-    /**
-     * 创建用户 id
-     */
-    private Long userId;
 
     private static final long serialVersionUID = 1L;
 }
