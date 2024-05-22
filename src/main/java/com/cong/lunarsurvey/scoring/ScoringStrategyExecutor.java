a9434772e949a201b6b767ca6a35c87e3b233417
lhccong <liuhuaicong@zhut.cn>
2024-05-22 09:02:24 +0800

    feat: 🚀新增评分策略执行器

---
+++ src/main/java/com/cong/lunarsurvey/scoring/ScoringStrategyExecutor.java
@@ -0,0 +1,48 @@
+package com.cong.lunarsurvey.scoring;
+
+import com.cong.lunarsurvey.common.ErrorCode;
+import com.cong.lunarsurvey.exception.BusinessException;
+import com.cong.lunarsurvey.model.entity.App;
+import com.cong.lunarsurvey.model.entity.UserAnswer;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * 评分策略执行器
+ */
+@Service
+public class ScoringStrategyExecutor {
+
+    // 策略列表
+    @Resource
+    private List<ScoringStrategy> scoringStrategyList;
+
+
+    /**
+     * 做分数
+     *
+     * @param choiceList 选择列表
+     * @param app        应用程序
+     * @return {@link UserAnswer }
+     * @throws Exception 例外
+     */
+    public UserAnswer doScore(List<String> choiceList, App app) throws Exception {
+        Integer appType = app.getAppType();
+        Integer appScoringStrategy = app.getScoringStrategy();
+        if (appType == null || appScoringStrategy == null) {
+            throw new BusinessException(ErrorCode.SYSTEM_ERROR, "应用配置有误，未找到匹配的策略");
+        }
+        // 根据注解获取策略
+        for (ScoringStrategy strategy : scoringStrategyList) {
+            if (strategy.getClass().isAnnotationPresent(ScoringStrategyConfig.class)) {
+                ScoringStrategyConfig scoringStrategyConfig = strategy.getClass().getAnnotation(ScoringStrategyConfig.class);
+                if (scoringStrategyConfig.appType() == appType && scoringStrategyConfig.scoringStrategy() == appScoringStrategy) {
+                    return strategy.doScore(choiceList, app);
+                }
+            }
+        }
+        throw new BusinessException(ErrorCode.SYSTEM_ERROR, "应用配置有误，未找到匹配的策略");
+    }
+}
