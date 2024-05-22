36706552c1a2eec9a2966ffad309aff1137adf65
lhccong <liuhuaicong@zhut.cn>
2024-05-22 08:57:22 +0800

    feat: 🚀新增自定义测评类应用评分策略

---
+++ src/main/java/com/cong/lunarsurvey/scoring/CustomTestScoringStrategy.java
@@ -0,0 +1,108 @@
+package com.cong.lunarsurvey.scoring;
+
+import cn.hutool.json.JSONUtil;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import com.cong.lunarsurvey.model.dto.question.QuestionContentDTO;
+import com.cong.lunarsurvey.model.entity.App;
+import com.cong.lunarsurvey.model.entity.Question;
+import com.cong.lunarsurvey.model.entity.ScoringResult;
+import com.cong.lunarsurvey.model.entity.UserAnswer;
+import com.cong.lunarsurvey.model.vo.QuestionVO;
+import com.cong.lunarsurvey.service.QuestionService;
+import com.cong.lunarsurvey.service.ScoringResultService;
+import org.jetbrains.annotations.NotNull;
+
+import javax.annotation.Resource;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 自定义测评类应用评分策略
+ *
+ * @author cong
+ * @date 2024/05/22
+ */
+@ScoringStrategyConfig(appType = 1, scoringStrategy = 0)
+public class CustomTestScoringStrategy implements ScoringStrategy {
+
+    @Resource
+    private QuestionService questionService;
+
+    @Resource
+    private ScoringResultService scoringResultService;
+
+    @Override
+    public UserAnswer doScore(List<String> choices, App app) throws Exception {
+        Long appId = app.getId();
+        // 1. 根据 id 查询到题目和题目结果信息
+        Question question = questionService.getOne(
+                Wrappers.lambdaQuery(Question.class).eq(Question::getAppId, appId)
+        );
+        List<ScoringResult> scoringResultList = scoringResultService.list(
+                Wrappers.lambdaQuery(ScoringResult.class)
+                        .eq(ScoringResult::getAppId, appId)
+        );
+
+        // 2. 统计用户每个选择对应的属性个数，如 I = 10 个，E = 5 个
+        // 初始化一个Map，用于存储每个选项的计数
+        Map<String, Integer> optionCount = new HashMap<>();
+
+        QuestionVO questionVO = QuestionVO.objToVo(question);
+        List<QuestionContentDTO> questionContent = questionVO.getQuestionContent();
+
+        // 遍历题目列表
+        for (QuestionContentDTO questionContentDTO : questionContent) {
+            // 遍历答案列表
+            for (String answer : choices) {
+                // 遍历题目中的选项
+                for (QuestionContentDTO.Option option : questionContentDTO.getOptions()) {
+                    // 如果答案和选项的key匹配
+                    if (option.getKey().equals(answer)) {
+                        // 获取选项的result属性
+                        String result = option.getResult();
+
+                        // 如果result属性不在optionCount中，初始化为0 在optionCount中增加计数
+                        optionCount.put(result, optionCount.getOrDefault(result, 0) + 1);
+                    }
+                }
+            }
+        }
+
+        // 3. 遍历每种评分结果，计算哪个结果的得分更高
+        // 初始化最高分数和最高分数对应的评分结果
+        int maxScore = 0;
+        ScoringResult maxScoringResult = scoringResultList.get(0);
+
+        // 遍历评分结果列表
+        for (ScoringResult scoringResult : scoringResultList) {
+            List<String> resultProp = JSONUtil.toList(scoringResult.getResultProp(), String.class);
+            // 计算当前评分结果的分数，[I, E] => [10, 5] => 15
+            int score = resultProp.stream()
+                    .mapToInt(prop -> optionCount.getOrDefault(prop, 0))
+                    .sum();
+
+            // 如果分数高于当前最高分数，更新最高分数和最高分数对应的评分结果
+            if (score > maxScore) {
+                maxScore = score;
+                maxScoringResult = scoringResult;
+            }
+        }
+
+        // 4. 构造返回值，填充答案对象的属性
+        return getUserAnswer(choices, app, appId, maxScoringResult);
+    }
+
+    private static @NotNull UserAnswer getUserAnswer(List<String> choices, App app, Long appId, ScoringResult maxScoringResult) {
+        UserAnswer userAnswer = new UserAnswer();
+        userAnswer.setAppId(appId);
+        userAnswer.setAppType(app.getAppType());
+        userAnswer.setScoringStrategy(app.getScoringStrategy());
+        userAnswer.setChoices(JSONUtil.toJsonStr(choices));
+        userAnswer.setResultId(maxScoringResult.getId());
+        userAnswer.setResultName(maxScoringResult.getResultName());
+        userAnswer.setResultDesc(maxScoringResult.getResultDesc());
+        userAnswer.setResultPicture(maxScoringResult.getResultPicture());
+        return userAnswer;
+    }
+}
